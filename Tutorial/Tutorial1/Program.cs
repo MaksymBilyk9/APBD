c7a4c84d0bcc8420424c45035cb55fadeae9029f
Maksym Bilyk <maksym.bilyk.business@gmail.com>
2025-03-06 12:43:55 +0100

    Change

--- Tutorial/Tutorial1/Program.cs
+++ Tutorial/Tutorial1/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Commit 1");
-Console.WriteLine("Commit 2");
-Console.WriteLine("Commit 2");
-Console.WriteLine("Commit 3");
-Console.WriteLine("Commit 3");
-Console.WriteLine("Commit 3");
+﻿void hello()
+{
+    Console.WriteLine("Hello World");
+}
+
+hello();
